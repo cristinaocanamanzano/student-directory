096ae7e5732a9c5632356b1cfaa8c5be31d4f97e
Cristina Maria Ocaña Manzano <cristinaocana1@gmail.com>
2018-08-28 20:37:51 +0000

    Chapter 8, Exercise 10

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.delete("\n", " \r", "\r\n")
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.delete("\n", " \r", "\r\n")
   end
   # return the array of students
   students
